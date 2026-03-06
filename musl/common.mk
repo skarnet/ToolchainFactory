04afc99ca51f0dea7ceec4ef8557d88530bc32aa
Laurent Bercot <ska-skaware@skarnet.org>
2026-03-06 21:18:10 +0000

    Add support for static pie, update to latest versions

--- musl/common.mk
+++ musl/common.mk
@@ -5,10 +5,12 @@ GCC_CONFIG += --disable-libquadmath --disable-decimal-float --disable-multilib
 GCC_CONFIG += --disable-nls --with-cloog=no --with-ppl=no --disable-libstdcxx-pch
 GCC_CONFIG += --disable-sjlj-exceptions --enable-gnu-unique-object --enable-linker-build-id
 GCC_CONFIG += CFLAGS='-g0 -O2' CXXFLAGS='-g0 -O2'
+MUSL_CONFIG += CFLAGS=-fPIE
 BINUTILS_CONFIG += --disable-gprofng
 
 ISL_SITE = https://libisl.sourceforge.io/
 GNU_SITE = https://ftp.gnu.org/pub/gnu
+# MUSL_REPO = https://git.musl-libc.org/cgit/musl
 
-DL_CMD = curl --no-progress-meter -o
+DL_CMD = curl --no-progress-meter -L -o
 # MAKE += LIMITS_H_TEST=true
